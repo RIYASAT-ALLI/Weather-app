550c415d49ab0fd0039bdf044806de95e0188a59
RIYASAT-ALLI <riyasatalli.it@gmail.com>
2026-01-07 11:23:47 +0530

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,32 @@
+const apiKey = "a6d83f0f5dc7f99840008da4d2de6981";
+
+function getWeather() {
+  const city = document.getElementById("cityInput").value.trim();
+  if (city === "") {
+    alert("Enter city name");
+    return;
+  }
+
+  fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}`)
+    .then(res => res.json())
+    .then(data => {
+      console.log(data);
+
+      if (data.cod !== 200) {
+        alert("City not found");
+        return;
+      }
+
+      document.getElementById("location").innerText = data.name;
+      document.getElementById("temp").innerText = Math.round(data.main.temp) + "°C";
+      document.getElementById("status").innerText = data.weather[0].main;
+      document.getElementById("humidity").innerText =
+        "Humidity: " + data.main.humidity + "%";
+      document.getElementById("wind").innerText =
+        "Wind: " + data.wind.speed + " km/h";
+    })
+    .catch(error => {
+      console.error(error);
+      alert("Error! Open console (F12)");
+    });
+}
